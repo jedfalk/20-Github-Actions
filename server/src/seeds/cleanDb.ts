5e4fc3a2f1d372630a1773820edaf84af556356a
JedFalk <jedfalk@gmail.com>
2025-06-09 18:53:23 -0500

    hopefully final cleandb fix

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -1,13 +1,19 @@
 import models from '../models/index.js';
 import db from '../config/connection.js';
 
-export default async (modelName: "Question", collectionName: string) => {
+export default async function cleanCollection(
+  modelName: keyof typeof models,
+  collectionName: string
+): Promise<void> {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
-      name: collectionName
-    }).toArray()
+    const model = models[modelName];
+    if (!model?.db?.db) {
+      throw new Error(`Model ${modelName} does not have a database connection.`);
+    }
 
-    if (modelExists.length) {
+    const modelExists = await model.db.db.listCollections({ name: collectionName }).toArray();
+
+    if (modelExists.length > 0) {
       await db.dropCollection(collectionName);
     }
   } catch (err) {
