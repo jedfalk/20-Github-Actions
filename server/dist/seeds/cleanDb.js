b8b3b1638ebb3d537f1899e99af7cf28925059f8
JedFalk <jedfalk@gmail.com>
2025-06-09 18:35:14 -0500

    cleandb fix

--- server/dist/seeds/cleanDb.js
+++ server/dist/seeds/cleanDb.js
@@ -1,15 +1,25 @@
 import models from '../models/index.js';
 import db from '../config/connection.js';
-export default async (modelName, collectionName) => {
-    try {
-        let modelExists = await models[modelName].db.db.listCollections({
-            name: collectionName
-        }).toArray();
-        if (modelExists.length) {
-            await db.dropCollection(collectionName);
-        }
+
+/**
+ * Drop a collection if it exists in the database.
+ * @param {keyof typeof models} modelName - The name of the model to use.
+ * @param {string} collectionName - The collection name to drop.
+ * @returns {Promise<void>}
+ */
+export default async function cleanCollection(modelName, collectionName) {
+  try {
+    const model = models[modelName];
+    if (!model?.db?.db) {
+      throw new Error(`Model '${modelName}' does not exist or has no db connection.`);
     }
-    catch (err) {
-        throw err;
+
+    const collections = await model.db.db.listCollections({ name: collectionName }).toArray();
+
+    if (collections.length) {
+      await db.dropCollection(collectionName);
     }
-};
+  } catch (err) {
+    throw err;
+  }
+}
